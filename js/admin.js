2f90714ee4177833ee9ef004e1895c0936cc913f
Shaikh Aftabuddin <53236880+SkAftabuddin@users.noreply.github.com>
2021-06-24 14:29:08 +0530

    Add files via upload

---
+++ js/admin.js
@@ -0,0 +1,41 @@
+// Graph
+var ctx = document.getElementById("myChart");
+
+var myChart = new Chart(ctx, {
+  type: "line",
+  data: {
+    labels: [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ],
+    datasets: [
+      {
+        data: [15245, 21345, 18483, 24003, 23489, 18670, 24092],
+        lineTension: 0,
+        backgroundColor: "transparent",
+        borderColor: "#007bff",
+        borderWidth: 4,
+        pointBackgroundColor: "#007bff",
+      },
+    ],
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: false,
+          },
+        },
+      ],
+    },
+    legend: {
+      display: false,
+    },
+  },
+});
